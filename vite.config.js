b2e38baf4f01449efaa9969641b73817ef942198
ArielQ1 <quizayaariel@gmail.com>
2025-06-15 23:15:10 -0400

    Refactor Bootstrap integration: add Bootstrap CSS link in index.html and remove import from main.jsx; update vite.config.js to externalize Bootstrap CSS

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PROYECTO-WEB-CARLOS/'
+  base: '/PROYECTO-WEB-CARLOS/',
+  build: {
+    rollupOptions: {
+      external: ['bootstrap/dist/css/bootstrap.min.css'],
+    },
+  },
 })
